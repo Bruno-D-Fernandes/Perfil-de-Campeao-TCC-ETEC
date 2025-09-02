6533dfbec4457e2845c32c7c3805bc842220a1de
maria <mariaalmeida131295s@gmail.com>
2025-09-02 00:01:53 -0300

    layout

--- pastaBolada/components/Configuracao.js
+++ pastaBolada/components/Configuracao.js
@@ -1,106 +1,49 @@
 import { Text, View, Pressable } from "react-native";
 
 export default function Configuracao({
-    titulo = "",
-    notificacao = "",
-    tema ="",
-    email = "",
-    senha ="",
-    autentifica ="",
-    sair ="",
-    excluir ="",
-    politicas ="",
-    termos ="",
-    saiba =""
+  titulo = "Configurações",
+  notificacao = "",
+  tema = "",
+  email = "",
+  senha = "",
+  autentifica = "",
+  sair = "",
+  excluir = "",
+  politicas = "",
+  termos = "",
+  saiba = ""
+}) {
+  return (
+    <View className="flex-1 p-4">
+      <View className="items-center mb-4">
+        <Text className="text-[#61D483] text-xl font-bold">{titulo}</Text>
+      </View>
+        <View className="flex-1 flex-col space-y-20 p-4">
 
-}){
-    return(
-        <View classname="flex-1 items-baseline">
-            <View classname="justify-items-center ">
-                <Text classname="">Configurações</Text>
+            <View className="p-4">
+            <Text className=" text-[#61D483]">Preferências</Text>
+            
+                <Pressable><Text className="text-[#4ADC76] font-bold">Notificações</Text></Pressable>
+                <Pressable><Text className="text-[#4ADC76] font-bold">Tema</Text></Pressable>
             </View>
-        <Text classname="font-bold">Preferências</Text>
 
-        <View classname="flex-1">
-            <View>
-                 <Pressable>
-                    <Text>
-                     Notificações
-                     </Text>
-                </Pressable>
-        </View>
-        <View>
-            <Pressable>
-                <Text>
-                Tema
-                </Text>
-            </Pressable>
-        </View>
+            <View className="p-4">
+            <Text className=" text-[#61D483] ">Conta</Text>
+                <Pressable><Text className="text-[#4ADC76] font-bold">Email</Text></Pressable>
+                 <Pressable><Text className="text-[#4ADC76] font-bold">Senha</Text></Pressable>
+                <Pressable><Text className="text-[#4ADC76] font-bold">Autentificação de 2 fatores</Text></Pressable>
+                 <Pressable><Text className="text-[#D46161] font-bold">Sair</Text></Pressable>
+                 <Pressable><Text className="text-[#D46161] font-bold">Excluir conta</Text></Pressable>
+            </View>
 
-        </View>
+            <View className="p-4">
+            <Text className=" text-[#61D483]">Sobre</Text>
+                <Pressable><Text className="text-[#4ADC76] font-bold">Políticas de privacidade</Text></Pressable>
+                <Pressable><Text className="text-[#4ADC76] font-bold">Termos e condições</Text></Pressable>
+                <Pressable><Text className="text-[#4ADC76] font-bold">Saiba mais</Text></Pressable>
+            </View>
 
-        <Text>Conta</Text>
-        <View classname="flex-1">
-            <View>
-                <Pressable>
-                    <Text>
-                    Email
-                    </Text>
-                </Pressable>
-            </View>
-            <View>
-                <Pressable>
-                    <Text>
-                    Senha
-                    </Text>
-                </Pressable>
-</View>
-<View>
-                <Pressable>
-                    <Text>
-                    Autentificação de 2 fatores
-                    </Text>
-                </Pressable>
-            </View>
-      <View>      
-                <Pressable>
-                    <Text>
-                    Sair
-                    </Text>
-                </Pressable>
-           </View>
-     <View>
-                <Pressable>
-                 <Text>
-                    Excluir conta
-                    </Text>
-                </Pressable>
-         </View>
-        </View>
-        <Text>Sobre</Text>
-        <View classname="flex-1">
-         <View>
-                <Pressable>
-                  <Text>
-                    Políticas de privacidade
-                    </Text>
-                </Pressable>
-           </View>
-            <View>
-                <Pressable>
-                    <Text>
-                    Termos e condições 
-                    </Text>
-                </Pressable>
-           </View>
-            <View>
-                <Pressable>
-                <Text>
-                    Saiba mais
-                    </Text>
-                </Pressable>
-            </View>
-        </View>
-        </View>
-    );
+      </View>
+    </View>
+  );
 }
