1a0d8420ef55c572dca5dda838b48976674aaf7f
KauCavalcante86 <kauad.cavalcante@gmail.com>
2025-09-04 01:14:10 -0300

    feat: font adionada splash

--- pastaBolada/screens/SplashScreen.js
+++ pastaBolada/screens/SplashScreen.js
@@ -1,11 +1,18 @@
 import { useEffect } from "react";
 import { View, Text, Image, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { useFonts, Poppins_400Regular, Poppins_700Bold, Poppins_500Medium } from "@expo-google-fonts/poppins";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import usuario from "./../../services/usuario";
 
 export default function SplashScreen() {
   const navigation = useNavigation();
+
+     const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+    Poppins_500Medium
+  });
 
   useEffect(() => {
     async function checkToken() {
@@ -22,7 +29,9 @@ export default function SplashScreen() {
       } catch (error) {
         console.error('Erro ao validar token:', error); // arrumar isso
       }
-    }
+    } 
+
+    
 
     checkToken();
   }, []);
@@ -34,16 +43,16 @@ export default function SplashScreen() {
         resizeMode="cover"
       />
 
-      <View className="w-full h-[350px] justify-evenly items-center">
-        <Text className="w-[80%] text-white text-[48px] leading-[90%]">
+      <View className="w-full h-[350px] justify-end items-center">
+        <Text className="w-[90%] text-white leading-[90%]" style={{fontFamily:'Poppins_500Medium', fontSize:40,}}>
           Pratique o esporte como você nunca viu.
         </Text>
 
-        <Pressable className="mt-4 w-[80%] rounded-[40px] h-[50px] bg-white rounded-full flex-row items-center justify-between" onPress={() => navigation.navigate('AuthStack', { screen: 'Login' })}>
-          <Text className="ml-5 text-[20px] text-[#2BEF66]">LET'S GO</Text>
+        <Pressable className="mt-8 mb-8 w-[80%] rounded-[40px] h-[50px] bg-white flex-row items-center justify-between" onPress={() => navigation.navigate('AuthStack', { screen: 'Login' })}>
+          <Text className="ml-5 text-[20px] text-[#2BEF66]" style={{fontFamily:'Poppins_500Medium'}}>LET'S GO</Text>
 
           <View className="h-10 w-10 rounded-full bg-[#2BEF66] justify-center items-center mr-1">
-            <Text className="text-[25px] text-white">&gt;</Text>
+            <Image className='ml-1.5' resizeMode="contain" style={{width:20, height:20}} source={require('../../assets/Splash/icon_proximo.png')} />
           </View>
         </Pressable>
 
